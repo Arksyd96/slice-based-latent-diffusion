ef771df08cf210628ae3af7605a471e2047327cf
Arksyd96 <aghiles.kebaili.1998@gmail.com>
2023-10-30 18:22:10 +0100

    sample script

--- sample.py
+++ sample.py
@@ -1,10 +1,15 @@
 import torch
 import argparse
 import sys
+import numpy as np
 
 from modules.models.embedders.latent_embedders import VAEGAN, VAE
 from modules.models.pipelines.diffusion_pipeline import DiffusionPipeline
 
+def replace_neg_pos(tensor, x, y):
+    tensor[tensor < 0] = x
+    tensor[tensor >= 0] = y
+    return tensor
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -15,6 +20,7 @@ if __name__ == "__main__":
                         'Standard deviation of the encoded latents in order to denormalize sampled latents')
     parser.add_argument('-n', '--num-samples', type=int, default=1, help='Number of samples to generate')
     parser.add_argument('-s', '--noise-shape', type=int, nargs='+', default=[1, 16, 16], help='Shape of the noise to sample')
+    parser.add_argument('-sb', '--slice-based', action='store_true', help='Whether to use slice-based latent diffusion or not', default=False)
     parser.add_argument('--ddim', action='store_true', help='Whether to use DDIM or not', default=False)
     parser.add_argument('--ddim-steps', type=int, default=50, help='Number of DDIM steps')
     parser.add_argument('--condition', type=float, nargs='+', default=None, help='Condition to use for sampling [v_0, v_1, ..., v_n]')
@@ -49,26 +55,40 @@ if __name__ == "__main__":
             use_ddim=args.ddim,
             steps=args.ddim_steps if args.ddim else 1000
         ).detach()
-        if args.noise_shape.__len__() == 3:
-            # slice-based latent diffusion; permute depth to batch for decoding
-            sample_volume = sample_volume.permute(0, 2, 1, 3, 4) # [N, C, D, H, W]
 
         sample_volume = sample_volume.mul(torch.tensor(args.std_norm, dtype=torch.float32, device=device))
-        
+
+        # => N, C, H, W, D
+        if args.slice_based:
+            # slice-based latent diffusion; permute depth to batch for decoding
+            sample_volume = sample_volume.permute(0, 4, 1, 2, 3)
+
         samples = []
         for idx in range(args.num_samples):
-            sample = sample_volume[idx, ...].permute(1, 0, 2, 3) # [D, C, H, W]
-            sample = diffuser.latent_embedder.decode(sample, emb=None).unsqueeze(0) # [1, D, C, H, W]
+            sample = sample_volume[idx, None, ...]
+            if args.slice_based:
+                sample = sample.squeeze(0)
+            sample = diffuser.latent_embedder.decode(sample, emb=None) 
+            if args.slice_based:
+                sample = sample.unsqueeze(0)
             samples.append(sample)
 
-        samples = torch.cat(samples, dim=0) # [N, D, C, H, W]
+        samples = torch.cat(samples, dim=0)
+        if args.slice_based:
+            samples = samples.permute(0, 2, 3, 4, 1)
+
+        print('Samples shape: {}'.format(samples.shape))
+
+        samples[:, 1] = replace_neg_pos(samples[:, 1], -1, 1) # rounding to -1, 1
+        N, C, W, H, D = samples.shape
 
     if args.save_png:
         from torchvision.utils import save_image
-        for idx in range(args.num_samples):
-            save_image(samples[idx, ...], '{}/sample_{}.png'.format(args.save_path, idx), nrow=samples.shape[1], normalize=True)
+        for idx in range(samples.shape[0]):
+            s = samples[idx, ...].permute(3, 0, 1, 2)
+            s = torch.vstack([s[:, c, ...] for c in range(s.shape[1])]).unsqueeze(1)
+            save_image(s, '{}/sample_{}.png'.format(args.save_path, idx), nrow=D, normalize=True)
 
-    samples = samples.permute(0, 2, 1, 3, 4) # [N, C, D, H, W]
     if args.save_volume:
         import numpy as np
         np.save('{}/samples.npy'.format(args.save_path), samples.cpu().numpy())
@@ -76,8 +96,7 @@ if __name__ == "__main__":
     if args.save_nifti:
         import nibabel as nib
         for idx in range(args.num_samples):
-            volume = samples[idx, ...].squeeze().cpu().numpy()
-            volume = volume.transpose(1, 2, 0)
+            volume = samples[idx, 0, ...].cpu().numpy()
             nib.save(nib.Nifti1Image(volume, np.eye(4)), '{}/sample_{}.nii.gz'.format(args.save_path, idx))
 
     print('Done!')
